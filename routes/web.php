b8995841e1cf0f4e47fb2fd53946bbb64adb85b3
Nadia <nadezhda.kropotova91@gmail.com>
2022-05-24 22:18:32 +0500

    page for category

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\MainController;
+use App\Http\Controllers\PublicCategoryController;
 use App\Http\Controllers\PublicPostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
